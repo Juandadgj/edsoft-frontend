7a4bd4ac067dec5335a0d9d3f397f26889ac68e2
Elzevasvj7 <sebastiancamero77@gmail.com>
2026-01-29 16:11:39 -0400

    fix: Set year styles

--- components/MainComponents/CopyYear.tsx
+++ components/MainComponents/CopyYear.tsx
@@ -1,4 +1,32 @@
+import { Checkbox } from "../ui/checkbox";
+import { useState } from "react";
+import { FieldValue } from "./forms/dinamyc-form/types/types";
+import { Button } from "../ui/button";
+
 function CopyYear() {
+  const [formData, setFormdata] = useState<Record<string, FieldValue>>({});
+  const options = [
+    {
+      label: "Asignaturas",
+      value: "asignaturas",
+    },
+    {
+      label: "Recomendaciones",
+      value: "recomendaciones",
+    },
+    {
+      label: "Logros",
+      value: "logros",
+    },
+    {
+      label: "Indicadores de Logros",
+      value: "indicatores",
+    },
+  ];
+
+  const handleChange = (e: any) => {
+    setFormdata({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
     <div className="w-full overflow-hidden h-full">
       <div className="mx-auto bg-white border-none border-2 shadow-2xl rounded-[10px] h-full py-4 px-2">
@@ -9,41 +37,33 @@ function CopyYear() {
             </strong>
           </h3>
         </div>
-        <div className="text-black h-full flex justify-center">
-          <div className="h-full border-white gap-1 w-full max-w-md">
-            <div className="form-control w-full">
-              <label className="label cursor-pointer flex justify-between gap-10">
-                <span className="label-text text-black">Asignaturas</span>
-                <input type="checkbox" defaultChecked />
-              </label>
-            </div>
-            <div className="form-control w-full">
-              <label className="label cursor-pointer flex justify-between gap-10">
-                <span className="label-text text-black">Recomendaciones</span>
-                <input type="checkbox" defaultChecked />
-              </label>
-            </div>
-            <div className="form-control w-full">
-              <label className="label cursor-pointer flex justify-between gap-10">
-                <span className="label-text text-black">Logros</span>
-                <input type="checkbox" defaultChecked />
-              </label>
-            </div>
-            <div className="form-control w-full">
-              <label className="label cursor-pointer flex justify-between gap-10">
-                <span className="label-text text-black">
-                  Indicadores de Logros
-                </span>
-                <input type="checkbox" defaultChecked />
-              </label>
-            </div>
+        <div className="p-4 rounded-md mb-3 flex justify-center items-center text-black">
+          <div className="w-full max-w-2xl border-2 border-main-blue p-4 rounded-md">
+            <p>
+              Usted puede hacer un copiado de los cursos del a�o inmediatamente
+              anterior, y puede escoger si se copia a la vez las asignaturas, los
+              logros o los indicadores de logros...
+            </p>
           </div>
         </div>
-        <div className="mx-auto bg-white border-2 shadow-2xl rounded-4xl p-5 flex justify-center flex-col">
+        <div className="text-black flex justify-center mb-3">
+          <div className="gap-1 w-full max-w-md grid grid-cols-1">
+            {options.map((option) => (
+              <Checkbox
+                key={option.value}
+                name={option.value}
+                value={option.value}
+                onChange={handleChange}
+                label={option.label}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-center flex-col">
           <div className="flex justify-center items-center my-2">
-            <button className="btn btn-sm h-10 border-none rounded-3xl text-white text-xs bg-[#0b5ed7] hover:bg-[#0b5ed7]">
+            <Button className="bg-[#0b5ed7] hover:bg-[#0b5ed7]">
               Generar copia
-            </button>
+            </Button>
           </div>
         </div>
       </div>
